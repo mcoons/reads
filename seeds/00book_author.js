243b8219abcf81a9f6ba46c7f8018fd0fff38969
Michael Coons <michael@Michaels-MacBook-Pro-4.local>
2018-08-27 17:15:01 -0600

    creted seeds

--- seeds/00book_author.js
+++ seeds/00book_author.js
@@ -3,13 +3,18 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('book_author').del()
   .then(() => {
-    return knex.raw("ALTER SEQUENCE student_id_seq RESTART WITH 1;");})
+    return knex.raw("ALTER SEQUENCE book_author_id_seq RESTART WITH 1;");})
     .then(function () {
       // Inserts seed entries
       return knex('book_author').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {book_id: 1, author_id:1},
+        {book_id: 1, author_id:2},
+        {book_id: 1, author_id:3},
+        {book_id: 2, author_id:4},
+        {book_id: 3, author_id:5},
+        {book_id: 4, author_id:6},
+        {book_id: 5, author_id:6},
+        {book_id: 6, author_id:6}
       ]);
     });
 };
