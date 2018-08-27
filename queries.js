faf39fa4187fef2723ad42b83f7f55fae7c4b613
Michael Coons <michael@Michaels-MacBook-Pro-4.local>
2018-08-27 17:16:50 -0600

    created basic join queries

---
+++ queries.js
@@ -0,0 +1,42 @@
+const database = require("./database-connection");
+
+module.exports = {
+
+//   list() {
+//     return database("book")
+//       .select();
+//   }
+
+list(){
+    return database("book_author")
+    .join('book', 'book.id', '=', 'book_author.book_id')
+    .join('author', 'author.id', '=', 'book_author.author_id')
+    .select('book_author.book_id', 
+            'book_author.author_id',
+            'book.title',
+            'book.genre',
+            'book.description',
+            'book.cover',
+            'author.first_name',
+            'author.last_name',
+            'author.biography',
+            'author.portrait')
+},
+
+authorByBook(book_id){
+    return database("book_author")
+    .join('author', 'author.id', '=', 'book_author.author_id')
+    .select('book_author.book_id', 
+            'book_author.author_id',
+            'book.title',
+            'book.genre',
+            'book.description',
+            'book.cover',
+            'author.first_name',
+            'author.last_name',
+            'author.biography',
+            'author.portrait')
+    .where('book_author.book_id',book_id)
+    }
+
+}
